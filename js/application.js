eb42f0430fbd4c12c8f5777acbc117ef72aaadbe
Teresa Mendez <teresa.mendez0@gmail.com>
2017-01-23 23:32:47 +1300

    Menu fijo animado

--- js/application.js
+++ js/application.js
@@ -1,2 +1,11 @@
-$(document).ready(function(){
-})
+$(window).on("scroll touchmove", function () {
+  var distance = $(document).scrollTop();
+  var $header = $('header');
+  
+  if (distance > 0){
+    $header.addClass('dark');
+  }
+  else{
+    $header.removeClass('dark');
+  }
+});
