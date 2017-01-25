c9cc5cadc0ec71f6fa2410385c8f2b402b01ff72
Teresa Mendez <teresa.mendez0@gmail.com>
2017-01-26 00:46:36 +1300

    Navbar mejorado

--- js/application.js
+++ js/application.js
@@ -1,17 +1,57 @@
-$(function () {
-    $.scrollUp({
-      scrollText: ''
-    });
+// Scrollup
+$.scrollUp({
+  scrollText: ''
 });
 
+// Change nav style after scrolling
 $(window).on("scroll touchmove", function () {
   var distance = $(document).scrollTop();
-  var $header = $('header');
+  var $nav = $('nav');
 
   if (distance > 0){
-    $header.addClass('dark');
+    $nav.addClass('dark');
   }
   else{
-    $header.removeClass('dark');
+    $nav.removeClass('dark');
+  }
+});
+
+// Smooth scroll for anchor links
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top - 38
+      }, 400);
+      return false;
+    }
+  }
+});
+
+// Hightlight anchor links on scroll
+var navChildren = $("nav li").children();
+var aArray = [];
+for (var i = 0; i < navChildren.length; i++) {
+  var aChild = navChildren[i];
+  var ahref = $(aChild).attr('href');
+  aArray.push(ahref);
+}
+$(window).scroll(function() {
+  var windowPos = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  var docHeight = $(document).height();
+  for (var i = 0; i < aArray.length; i++) {
+    var theID = aArray[i];
+    var secPosition = $(theID).offset().top;
+    secPosition = secPosition - 80;
+    var divHeight = $(theID).height();
+    divHeight = divHeight + 90;
+    if (windowPos >= secPosition && windowPos < (secPosition + divHeight)) {
+      $("a[href='" + theID + "']").addClass("active");
+    } else {
+      $("a[href='" + theID + "']").removeClass("active");
+    }
   }
 });
